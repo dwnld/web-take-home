0a799e2004ed40e9e0de9038b5d799232dbbd054
Geoff Liu <hi@geoffliu.me>
2016-09-28 12:14:22 -0400

    Skeleton code + React starter

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# web-take-home
+# dwnld take-hom interview
+
+Hi! We are so glad that you've decided to take on this interview with us. Here are the steps to get started.
+
+1. You will need Node.js and NPM. Install them if you haven't already.
+2. Clone this repository, and go to the directory.
+3. Run `npm install`
+4. Run `npm run watch`
+
+The last command will start up a server on your local port 3000. Go to it, and
+you should see a placeholder page. The watch command also sets up monitoring of
+any changes to the JS files in the `lib` directory. The file `lib/app.js` is the
+entry point, and it has already been included in the HTML file.
+
+Some rules for this interview:
+
+- You're welcome to use any open source Javascript libraries.
+- You're welcome to use Stack Overflow, but cite your sources.
+- CSS libraries/frameworks are *NOT* allowed.
+
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const uuid = require('uuid')
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/assets/index.html')
+})
+
+app.use('/assets', express.static(__dirname + '/assets'))
+
+const messages = [{
+  subject: "Hi!",
+  body: "something"
+}, {
+  subject: "And the winner is...",
+  body: "Jim"
+}]
+
+const messagesWithIds = messages.map(function (msg) {
+  return Object.assign({}, msg, { id: uuid.v4() })
+})
+
+app.get('/api/messages', function (req, res) {
+  const resp =  messagesWithIds.map(function (msg) {
+    return { id: msg.id, subject: msg.subject }
+  })
+  res.send(resp)
+})
+
+app.get('/api/messages/:messageId', function (req, res) {
+  const message = messagesWithIds.find(function (msg) {
+    return msg.id === req.params.messageId
+  })
+  if (message)
+    res.send(message)
+  else
+    res.status(404).send()
+})
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000')
+})
