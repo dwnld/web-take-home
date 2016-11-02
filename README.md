0fdfcffaca3f43cee2b38ad2fedcfd3fb2ed6406
charlesoconor <coconor@umich.edu>
2016-11-02 18:47:56 -0400

    changing screen shot

--- README.md
+++ README.md
@@ -5,12 +5,43 @@ Hi! We are so glad that you've decided to take on this interview with us. Here a
 1. You will need Node.js and NPM. Install them if you haven't already.
 2. Clone this repository, and go to the directory.
 3. Run `npm install`
-4. Run `npm run watch`
+4. Run `npm run start`
 
 The last command will start up a server on your local port 3000. Go to it, and
 you should see a placeholder page. The watch command also sets up monitoring of
 any changes to the JS files in the `lib` directory. The file `lib/app.js` is the
 entry point, and it has already been included in the HTML file.
+
+We are building an application to view messages. The requirements are to display a
+list of messages. The user can click on each to view the body. It can look like the 
+following, but be as creative as possible. This is only here to get you started and
+is pretty bare bones.
+
+
+![Image](screenshots/exampleScreenShot.png)
+
+The server has been set up with these endpoints for you 
+
+Get all messages
+* `/api/messages`
+```javascript
+[
+  {
+     id:<integer>,
+     subject:<string>
+  }
+]
+```
+
+Get body of message for the id 
+* `/api/messages/:messageId`
+
+```javascript
+{
+  subject:<string>,
+  body:<string>
+}
+```
 
 Some rules for this interview:
 
--- server.js
+++ server.js
@@ -12,10 +12,39 @@ app.use('/assets', express.static(__dirname + '/assets'))
 const messages = [{
   subject: "Hi!",
   body: "something"
-}, {
+}, 
+  {
   subject: "And the winner is...",
   body: "Jim"
-}]
+},
+  {
+  subject: "Re: And the winner is...  ",
+  body: "Jim"
+},
+  { 
+    subject:"Payment Protection Insurance (PPI)",
+    body:"IMPORTANT - You could be entitled up to £3,160 in compensation from mis-sold PPI on a credit card or loan. Please reply PPI for info or STOP to opt out."
+  },
+  { 
+    subject:"Quick Loans",
+    body:"A [redacted] loan for £950 is approved for you if you receive this SMS. 1 min verification & cash in 1 hr at www.[redacted].co.uk to opt out reply stop"
+  },
+  { 
+    subject:"Accident compensation",
+    body:"You have still not claimed the compensation you are due for the accident you had. To start the process please reply YES. To opt out text STOP"
+  },
+  { 
+    subject:"Debt forgiveness",
+    body:"Due to a new legislation, those struggling with debt can now apply to have it written off. For more information text the word INFO or to opt out text STOP"
+  },
+  { 
+    subject:"Pension reviews",
+    body:"Our records indicate your Pension is under performing to see higher growth and up to 25% cash release reply PENSION for a free review. To opt out reply STOP"
+  },
+  {
+    subject: "You've won a million dollars",
+    body: "you just need to wire me the $1000 to process it"
+  }]
 
 const messagesWithIds = messages.map(function (msg) {
   return Object.assign({}, msg, { id: uuid.v4() })
@@ -41,3 +70,4 @@ app.get('/api/messages/:messageId', function (req, res) {
 app.listen(3000, function () {
   console.log('Server listening on port 3000')
 })
+
